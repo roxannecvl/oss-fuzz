ca23eddb60c8f17e9d2582af71b7f5d05c4d3780
ayahamane <aya.hamane@epfl.ch>
2025-04-17 18:11:40 +0000

    clean script_setup

--- scripts/script_setup.sh
+++ scripts/script_setup.sh
@@ -1,5 +1,2 @@
-# First cript for setup
-# Ensure we are cloning our fork of oss-fuzz to ensure /infra/helper.py is updated 
-# with 'timeout', '4h' at line 1445
 git clone git@github.com:roxannecvl/oss-fuzz.git && cd oss-fuzz
 python3 infra/helper.py build_image libjpeg-turbo
