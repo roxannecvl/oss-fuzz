8e0c7e22c3ec6e9f4b420b21f5ee720fb492e3db
ayahamane <aya.hamane@epfl.ch>
2025-04-24 18:57:08 +0000

    testing with decompress again

--- scripts/coverage_no_seed.sh
+++ scripts/coverage_no_seed.sh
@@ -1,3 +1,3 @@
 cd oss-fuzz
 python3 infra/helper.py build_fuzzers --sanitizer coverage libjpeg-turbo
-python3 infra/helper.py coverage libjpeg-turbo --corpus-dir build/out/corpus_no_seed/ --fuzz-target compress_fuzzer
+python3 infra/helper.py coverage libjpeg-turbo --corpus-dir build/out/corpus_no_seed/ --fuzz-target decompress_yuv_fuzzer
