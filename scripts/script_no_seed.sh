788b81191eed22e4d306fe113b23fa7adedf19a9
yoan <yoan@laptop>
2025-04-28 19:09:07 +0200

    replace used fuzzer

--- scripts/script_no_seed.sh
+++ scripts/script_no_seed.sh
@@ -5,4 +5,4 @@ rm -rf build/out/libjpeg-turbo/*
 python3 infra/helper.py build_fuzzers libjpeg-turbo
 mkdir -p build/out/corpus_no_seed/
 # Store results to build/out/corpus_no_seed
-python3 infra/helper.py run_fuzzer libjpeg-turbo compress_fuzzer --corpus-dir build/out/corpus_no_seed
+python3 infra/helper.py run_fuzzer libjpeg-turbo libjpeg_turbo_fuzzer --corpus-dir build/out/corpus_no_seed
