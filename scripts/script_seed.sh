c7d68d8500557acf7423da4e5248d5eff8189790
ayahamane <aya.hamane@epfl.ch>
2025-05-01 09:17:21 +0000

    me cleaning my local repo (ignore)

--- scripts/script_seed.sh
+++ scripts/script_seed.sh
@@ -5,4 +5,4 @@ rm -rf build/out/libjpeg-turbo/*
 python3 infra/helper.py build_fuzzers libjpeg-turbo
 mkdir -p build/out/corpus_seed/
 # Store results to build/out/corpus_seed
-python3 infra/helper.py run_fuzzer libjpeg-turbo decompress_yuv_fuzzer --corpus-dir build/out/corpus_seed
+python3 infra/helper.py run_fuzzer libjpeg-turbo libjpeg_turbo_fuzzer  --corpus-dir build/out/corpus_seed
