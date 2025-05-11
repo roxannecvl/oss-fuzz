bd9daa20b3baa691af057295bae558555e120623
Roxanne Chevalley <125833841+roxannecvl@users.noreply.github.com>
2025-05-11 19:58:44 +0200

    Create full_script_no_seed.sh

---
+++ scripts/full_script_no_seed.sh
@@ -0,0 +1,12 @@
+python3 infra/helper.py build_image libjpeg-turbo
+git checkout no-seed
+
+rm -rf build/out/libjpeg-turbo/*
+python3 infra/helper.py build_fuzzers libjpeg-turbo
+mkdir -p build/out/corpus_no_seed/
+# Store results to build/out/corpus_no_seed
+python3 infra/helper.py run_fuzzer libjpeg-turbo libjpeg_turbo_fuzzer --corpus-dir build/out/corpus_no_seed
+
+# Get coverage
+python3 infra/helper.py build_fuzzers --sanitizer coverage libjpeg-turbo
+python3 infra/helper.py coverage libjpeg-turbo --corpus-dir build/out/corpus_no_seed/ --fuzz-target libjpeg_turbo_fuzzer
