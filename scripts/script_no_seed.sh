b74fb3d956c808d9f685ff7f91806207bee74176
Roxanne Chevalley <125833841+roxannecvl@users.noreply.github.com>
2025-05-11 19:55:28 +0200

    Delete scripts/script_no_seed.sh

--- scripts/script_no_seed.sh
+++
@@ -1,8 +0,0 @@
-# Script without seed
-cd oss-fuzz
-git checkout no-seed
-rm -rf build/out/libjpeg-turbo/*
-python3 infra/helper.py build_fuzzers libjpeg-turbo
-mkdir -p build/out/corpus_no_seed/
-# Store results to build/out/corpus_no_seed
-python3 infra/helper.py run_fuzzer libjpeg-turbo libjpeg_turbo_fuzzer --corpus-dir build/out/corpus_no_seed
