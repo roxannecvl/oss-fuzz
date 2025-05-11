0d53af251924f30bf12e7f65a85e13e0eda5f850
Roxanne Chevalley <125833841+roxannecvl@users.noreply.github.com>
2025-05-11 20:17:15 +0200

    Delete scripts/coverage_seed.sh

--- scripts/coverage_seed.sh
+++
@@ -1,3 +0,0 @@
-cd oss-fuzz
-python3 infra/helper.py build_fuzzers --sanitizer coverage libjpeg-turbo
-python3 infra/helper.py coverage libjpeg-turbo --corpus-dir build/out/corpus_seed/ --fuzz-target transform_fuzzer
