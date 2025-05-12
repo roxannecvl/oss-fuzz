99d7603df7ec53130601d79de11d68e33111add2
yoan <yoan@laptop>
2025-05-12 10:48:24 +0200

    add full script save_image

---
+++ scripts/full_script_save_image.sh
@@ -0,0 +1,14 @@
+# Suposed to be ran from the oss-fuzz root directory
+python3 infra/helper.py build_image libjpeg-turbo
+git checkout tj3SaveImage
+rm -rf build/out/libjpeg-turbo/*
+# Download the new seed corpus
+wget -O projects/libjpeg-turbo/seed_generation/save_image_corpus.zip  https://paultissot.ch/save_image_corpus.zip
+python3 infra/helper.py build_fuzzers libjpeg-turbo
+mkdir -p build/out/corpus_seed/
+# Store results to build/out/corpus_seed
+python3 infra/helper.py run_fuzzer libjpeg-turbo save_image_fuzzer --corpus-dir build/out/corpus_seed
+
+# Get coverage
+python3 infra/helper.py build_fuzzers --sanitizer coverage libjpeg-turbo
+python3 infra/helper.py coverage libjpeg-turbo --corpus-dir build/out/corpus_seed/ --fuzz-target save_image_fuzzer
