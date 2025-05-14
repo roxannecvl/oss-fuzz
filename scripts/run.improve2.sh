8e9472f28d08bd118ebe25e2e7c2398b2b0cc071
yoan <yoan@laptop>
2025-05-14 11:35:59 +0200

    improve 2 create seed corpus dir

--- scripts/run.improve2.sh
+++ scripts/run.improve2.sh
@@ -4,6 +4,7 @@ python3 infra/helper.py build_image libjpeg-turbo
 git checkout tj3SaveImage
 rm -rf build/out/libjpeg-turbo/*
 # Download the new seed corpus
+mkdir projects/libjpeg-turbo/seed_generation
 wget -O projects/libjpeg-turbo/seed_generation/save_image_corpus.zip  https://paultissot.ch/save_image_corpus.zip
 python3 infra/helper.py build_fuzzers libjpeg-turbo
 mkdir -p build/out/corpus_seed/
